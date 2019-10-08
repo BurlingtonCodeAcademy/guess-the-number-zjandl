95246082e559dfe5eb1d1fff42c75349b935fa13
Zach Jandl <zachjandl@gmail.com>
2019-10-08 17:58:40 -0400

    added reverse guess game

--- index.js
+++ index.js
@@ -42,3 +42,16 @@ async function secondGuess() {
       };
 
     }}}
+
+// This is the reverse number guess game I created
+// console.log ("I'm thinking of a number between 1 and 100\nTake a guess!");
+// process.stdin.on ('data', (chunk) => {
+// let number = chunk;
+// if (number > 20) {
+//  console.log("Sorry! The number I'm thinking of is lower than " + number);
+//  } else if (number < 20) {
+//          console.log("Sorry! The number I'm thinking of is higher than " + number);
+//      } else {
+//  console.log ("Correct! How'd you know?");} 
+//  
+// });
