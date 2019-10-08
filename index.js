44dae0c8232931201ea32aa220c7d4ab65017095
Zach Jandl <zachjandl@gmail.com>
2019-10-08 18:57:56 -0400

    made code more better

--- index.js
+++ index.js
@@ -6,6 +6,33 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function secondGuess() {
+  let tryAgain = await ask("Higher or Lower or Correct?  ");
+  if (tryAgain.toLowerCase() === "lower") {
+    await ask("How about " + getRandomIntInclusive(min, max) + "? ");
+  } else if (tryAgain.toLowerCase() === "higher") {
+    await ask("How about " + getRandomIntInclusive(min, max) + "? ");
+  } else if (tryAgain.toLowerCase() === "correct") {
+    console.log("I'm so smart!!!");
+  }
+
+};
+async function guessNum() {
+  let firstGuess = await ask("Are you thinking of the number..." + computerGuess + "? \nYes or No? ");
+  if (firstGuess.toLowerCase() === "correct") {
+    console.log("I'm so smart!!!");
+  } else if (firstGuess.toLowerCase() === "no") {
+
+    secondGuess()
+
+  }
+}
 
 start();
 
@@ -14,34 +41,30 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
 
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  let min = 1
+  let max = 100
+  let computerGuess = getRandomIntInclusive(min, max)
 
-guessNum()
+  // guessNum()
 
-async function guessNum() {
-  let firstGuess = await ask ("Are you thinking of the number..." + getRandomIntInclusive(1, 100) + "? \nYes or No? ");
-    if (firstGuess.toLowerCase() === "correct") {
-        console.log ("I'm so smart!!!");
-    } else if (firstGuess.toLowerCase() === "no") { 
+  let firstGuess = await ask("Are you thinking of the number..." + computerGuess + "? \nYes or No? ");
+  while (firstGuess === "no") {
+    let tryAgain = await ask("Higher or Lower or Correct?  ");
+    if (tryAgain.toLowerCase() === "lower") {
+      max = (computerGuess - 1)
+    } else if (tryAgain.toLowerCase() === "higher") {
+      min = (computerGuess + 1)
+    }
 
-secondGuess()
+    computerGuess = getRandomIntInclusive(min, max)
+    firstGuess = await ask("How about " + computerGuess + "? Yes on No ");
+if (firstGuess === "yes"){
+  console.log("I'm so smart!")
+  process.exit()
+}
+  }
 
-async function secondGuess() {
-    let tryAgain = await ask ("Higher or Lower or Correct?  ");
-        while (tryAgain.toLowerCase() === "lower") {
-            await ask ("How about " + getRandomIntInclusive(1, 100) + "? ");
-        } while (tryAgain.toLowerCase() === "higher") {
-                await ask ("How about " + getRandomIntInclusive(1, 100) +"? ");
-            } if (tryAgain.toLowerCase() === "correct") {
-        console.log ("I'm so smart!!!");}
-        
-      };
-
-    }}}
+}
 
 // This is the reverse number guess game I created
 // console.log ("I'm thinking of a number between 1 and 100\nTake a guess!");
